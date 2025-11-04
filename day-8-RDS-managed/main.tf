67987d4f58dfb38a3810a468c28d002677ec236f
yogithakandi <kandhiyogitha29@gmail.com>
2025-11-04 15:22:36 +0530

    managed

---
+++ day-8-RDS-managed/main.tf
@@ -0,0 +1,50 @@
+resource "aws_db_instance" "default" {
+  allocated_storage       = 10
+   identifier =             "book-rds"
+  db_name                 = "mydb"
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t3.micro"
+  manage_master_user_password = true #rds and secret manager manage this password
+  username                    = "admin"
+  db_subnet_group_name    = aws_db_subnet_group.sub-grp.id
+  parameter_group_name    = "default.mysql8.0"
+  backup_retention_period  = 7   # Retain backups for 7 days
+  backup_window            = "02:00-03:00" # Daily backup window (UTC)
+
+  # Enable performance insights
+#   performance_insights_enabled          = true
+#   performance_insights_retention_period = 7  # Retain insights for 7 days
+  maintenance_window = "sun:04:00-sun:05:00"  # Maintenance every Sunday (UTC)
+  deletion_protection = true
+  skip_final_snapshot = true
+  depends_on = [ aws_db_subnet_group.sub-grp ]
+}
+
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "dev"
+    }
+  
+}
+resource "aws_subnet" "subnet-1" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "us-east-1a"
+  
+}
+resource "aws_subnet" "subnet-2" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1b"
+  
+}
+resource "aws_db_subnet_group" "sub-grp" {
+  name       = "mycutsubnet1"
+  subnet_ids = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
